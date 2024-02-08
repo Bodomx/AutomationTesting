af0bf5ddc500c4eabfcefe7251695263e964ed25
jzsCodegame <jzsplayground@gmail.com>
2024-02-07 21:38:39 -0500

    4th commit

--- src/test/java/SeleniumTestMethods/Actionsmethods.java
+++ src/test/java/SeleniumTestMethods/Actionsmethods.java
@@ -4,130 +4,82 @@ import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.testng.AssertJUnit;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 
 import Pages.Controler;
 
 public class Actionsmethods extends Controler {
-	
-	@Test(priority = 0)
-	public void actionset1() throws InterruptedException {
-		
-	   driver.get("https://rahulshettyacademy.com/dropdownsPractise/");Thread.sleep(1000);
-		
-		//driver.get("http://spicejet.com");
-		
-		driver.navigate().refresh(); Thread.sleep(1000); // refresh page
+    private WebDriverWait wait;
+    private Actions action;
 
-		driver.manage().window().maximize(); Thread.sleep(1000); // maximize window
+    @BeforeMethod
+    public void setup() {
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 30);
+        action = new Actions(driver);
+    }
 
-		driver.manage().deleteAllCookies(); Thread.sleep(1000); // delete all cookies
+    @Test(priority = 0)
+    public void actionset1() {
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.navigate().refresh();
 
-		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS); // page load timeout
+        WebElement addup1 = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("hrefIncAdt")));
+        Action seriesOfActions1 = action.moveToElement(addup1).click().build();
 
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); // implicit wait
+        for(int i=1;i<10;i++) {
+            seriesOfActions1.perform();
+        }
 
-		//Ex = new WebDriverWait(driver, 30); // explicit wait
-		
-		driver.findElement(By.id("divpaxinfo")).click(); Thread.sleep(1000);
-		
-		WebElement addup1 = driver.findElement(By.id("hrefIncAdt")); // the plus buton for adults
-		
-		 Actions action1 = new Actions(driver);
-		 
-		 Action seriesOfActions1 = action1.moveToElement(addup1).click().build();// 
-		
-		addup1 = Ex.until(ExpectedConditions.presenceOfElementLocated(By.id("hrefIncAdt")));
-		 
-		for(int i=1;i<10;i++)   // Execute Line for 9 times
-			   {
-			   seriesOfActions1.perform(); Thread.sleep(1000);
-			   } 
-		  
-		int j=1;
-		while(j<10){
-			
-		driver.findElement(By.id("hrefIncChd")).click(); Thread.sleep(1000); // using xpath or findElement scripting click 8 times
-		j++;
-		}
-	}
-			 	 
-			 
-	
-	@Test(priority = 1)
-	public void actionset2() throws InterruptedException {
-	
-	            //driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-		 
-		 //driver.findElement(By.id("divpaxinfo")).click();
-		 
-		 System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
-		 
-		 Thread.sleep(1000);
-		 
-		 WebElement substract = driver.findElement(By.id("hrefDecAdt")); // the plus buton for adults
-		 
-		 Actions action = new Actions(driver);
-		 
-		 Action seriesOfActions = action.moveToElement(substract).click().build();// 
-			
-		//seriesOfActions.perform();
-		 
-			for(int i=1;i<5;i++)
+        int j=1;
+        while(j<10){
+            driver.findElement(By.id("hrefIncChd")).click();
+            j++;
+        }
+    }
 
-			{
+    @Test(priority = 1)
+    public void actionset2() {
+        WebElement substract = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("hrefDecAdt")));
+        Action seriesOfActions = action.moveToElement(substract).click().build();
 
-			
-				seriesOfActions.perform();// Using mouse actions 
+        for(int i=1;i<5;i++) {
+            seriesOfActions.perform();
+        }
 
-			}
-		          driver.findElement(By.id("btnclosepaxoption"));
+        Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult", "Expected 5 Adults but found different value");
 
-			AssertJUnit.assertEquals(driver.findElement(By.id("divpaxinfo")).getText() ,"5 Adult");
+        WebElement search = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("autosuggest")));
+        Action seriesOfActions2 = action.moveToElement(search).click().sendKeys("Bang").sendKeys(Keys.ARROW_DOWN).build();
+        seriesOfActions2.perform();
+    }
 
-			System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
-			
-			WebElement search = driver.findElement(By.id("autosuggest"));
-			
-			Action seriesOfActions2 = action.moveToElement(search).click().sendKeys("Bang").sendKeys(Keys.ARROW_DOWN).build();
-			
-			seriesOfActions2.perform();
-			
-	}
-	@Test(priority = 3)
-	  public void actionbuilder() {
-		  String baseUrl = "http://www.facebook.com/";
-		  
+    @Test(priority = 3)
+    public void actionbuilder() {
+        String baseUrl = "http://www.facebook.com/";
+        driver.get(baseUrl);
+        WebElement txtUsername = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("email")));
 
-		  driver.get(baseUrl);
-		  WebElement txtUsername = driver.findElement(By.id("email"));
+        Action seriesOfActions = action
+            .moveToElement(txtUsername)
+            .click()
+            .keyDown(txtUsername, Keys.SHIFT)
+            .sendKeys(txtUsername, "hello")
+            .keyUp(txtUsername, Keys.SHIFT)
+            .doubleClick(txtUsername)
+            .contextClick()
+            .build();
 
-		  Actions builder = new Actions(driver);
-		  Action seriesOfActions = builder
-		  .moveToElement(txtUsername)
-		  .click()
-		  .keyDown(txtUsername, Keys.SHIFT)
-		  .sendKeys(txtUsername, "hello")
-		  .keyUp(txtUsername, Keys.SHIFT)
-		  .doubleClick(txtUsername)
-		  .contextClick()
-		  .build();
-		  for(int i=1;i<5;i++)
-
-			{
-
-			
-			  seriesOfActions.perform() ;// Using mouse actions 
-
-			}
-		  
-		  }
+        for(int i=1;i<5;i++) {
+            seriesOfActions.perform();
+        }
+    }
 }
