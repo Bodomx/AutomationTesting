228621bc6e3fe47133c4f2201b5f6ad967b29d22
jzsCodegame <jzsplayground@gmail.com>
2024-02-27 21:37:44 -0500

    3nd Commit

--- src/test/java/SeleniumTestMethods/Alerts.java
+++ src/test/java/SeleniumTestMethods/Alerts.java
@@ -1,53 +1,44 @@
 package SeleniumTestMethods;
 
-import org.openqa.selenium.By;
-import org.testng.annotations.Test;
+import org.openqa.selenium.By; // Importing By class from Selenium
+import org.testng.annotations.Test; // Importing Test annotation from TestNG
+
+import Pages.Controler; // Importing Controler class from Pages package
+
+public class Alerts extends Controler { // Class Alerts extending Controler class
+
+ @Test(priority = 0) // Test annotation with priority 0
+ public void alerts() throws InterruptedException { // Method alerts which throws InterruptedException
+
+  driver.get("https://rahulshettyacademy.com/AutomationPractice/"); // Navigating to the URL
+
+  driver.findElement(By.id("name")).click(); // Clicking on the element with id "name"
+
+  driver.findElement(By.id("name")).sendKeys("Ra"); // Sending keys "Ra" to the element with id "name"
+
+  driver.findElement(By.cssSelector("[id='alertbtn']")).click(); // Clicking on the element with id "alertbtn"
+
+  Thread.sleep(3000); // Pausing the execution for 3 seconds
+
+  System.out.println(driver.switchTo().alert().getText()); // Printing the text of the alert
+
+  driver.switchTo().alert().accept(); // Accepting the alert
+
+  driver.findElement(By.id("confirmbtn")).click(); // Clicking on the element with id "confirmbtn"
+
+  System.out.println(driver.switchTo().alert().getText()); // Printing the text of the alert
+
+  driver.switchTo().alert().accept(); // Accepting the alert
+ }
 
 
-import Pages.Controler;
+ @Test(priority = 1) // Test annotation with priority 1
+ public void alerts2() throws InterruptedException{ // Method alerts2 which throws InterruptedException
 
-public class Alerts extends Controler {
-
-	
-	@Test(priority = 0)
-	public void alerts() throws InterruptedException {
-		// TODO Auto-generated method stub
-		
-
-		//WebDriver driver = new ChromeDriver();
-
-		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\jzo_0\\eclipse-workspace\\alertPractice\\chromedriver-windows.exe");
-		
-		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-		
-		driver.findElement(By.id("name")).click();
-
-		driver.findElement(By.id("name")).sendKeys("Ra");
-
-		driver.findElement(By.cssSelector("[id='alertbtn']")).click();
-		
-		Thread.sleep(3000);
-
-		System.out.println(driver.switchTo().alert().getText());
-
-		driver.switchTo().alert().accept();
-
-		driver.findElement(By.id("confirmbtn")).click();
-
-		System.out.println(driver.switchTo().alert().getText());
-		
-		driver.switchTo().alert().accept();
-	}
-	
-	
-	@Test(priority = 1)
-	
-	public void alerts2() throws InterruptedException{
-		
-		driver.get("C:\\Tools\\workspace1\\Aut_Practice_April_2023\\Frames\\Alert.html");
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("/html/body/button[1]")).click();
-	    Thread.sleep(2000);
-	    driver.switchTo().alert().accept();
-	}
+  driver.get("C:\\Tools\\workspace1\\Aut_Practice_April_2023\\Frames\\Alert.html"); // Navigating to the local file
+  Thread.sleep(2000); // Pausing the execution for 2 seconds
+  driver.findElement(By.xpath("/html/body/button[1]")).click(); // Clicking on the first button in the body
+  Thread.sleep(2000); // Pausing the execution for 2 seconds
+  driver.switchTo().alert().accept(); // Accepting the alert
+ }
 }
